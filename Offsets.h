7ba752dd6ad6fe3f5cebd17868648c58657a8ded
Taob-arch <77207095+Taob-arch@users.noreply.github.com>
2021-08-08 23:54:32 +0100

    Create Offsets.h

---
+++ Offsets.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define convar_fov 0xA04C54
+#define convar_fpsMax 0x6B3AA4
+
+//only just started documenting these, if youcan, please support the projectby finding and submitting your offsets!
