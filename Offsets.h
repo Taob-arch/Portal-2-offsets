ba03fe80a4dbd5e92f56483e4b57314d1c7a41ab
Taob-arch <77207095+Taob-arch@users.noreply.github.com>
2021-08-09 10:16:47 +0100

    additions

--- Offsets.h
+++ Offsets.h
@@ -1,6 +1,19 @@
 #pragma once
 
-#define convar_fov 0xA04C54
-#define convar_fpsMax 0x6B3AA4
-
+#define convar_cl_fov 0xA04C54                                              //client.dll
+#define convar_fps_max 0x6B3AA4                                              //client.dll
+#define convar_cc_subtitles 0x9E1C8C                                        //client.dll
+#define convar_cl_ambient_light_disableentities 0x99BEB4                    //client.dll
+#define convar_cl_autowepswitch 0xA080A4                                    //client.dll
+#define convar_cl_backspeed 0x9E51EC                                        //client.dll
+#define convar_cl_drawhud 0x9DBB64                                          //client.dll
+#define convar_cl_drawmonitors 0x9F6C64                                     //client.dll
+#define convar_cl_drawshadowtexture 0x9F3A4C                                //client.dll
+#define convar_cl_mouselook_roll_compensation 0x9E79E4                      //client.dll
+#define convar_cl_portal_camera_orientation_max_speed 0xA046D4              //client.dll
+#define convar_cl_portal_camera_orientation_acceleration_rate 0xA047DC      //client.dll
+#define convar_cl_showfps 0x9F1EEC                                          //client.dll
+#define convar_crosshair 0x9E1F44                                           //client.dll
+#define convar_displaysoundlist 0x7F4B84                                    //server.dll
+#define convar_sv_gravity 0x9EB414                                          //client.dll
 //only just started documenting these, if youcan, please support the projectby finding and submitting your offsets!
